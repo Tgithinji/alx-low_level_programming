2c291ad3e8f9e8394e6cb0e589058d1218bf0374
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-03-17 03:00:02 +0300

    Print output to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints output to standard error
+ *
+ * Returns: Always 1 (Success)
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61, 1, stderr);
+	retun (1);
+}
