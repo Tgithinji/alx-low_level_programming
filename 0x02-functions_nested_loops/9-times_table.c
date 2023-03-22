084ee47784b97c488807cde563ed27db01d84d9e
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-03-23 01:06:34 +0300

    Fixed spacing error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,16 +11,18 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar('0');
 		for (j = 0; j < 10; j++)
 		{
-			k = i * j;
+			k = i * j;	
+			if (j == 0)
+				_putchar(k + '0');
 			_putchar(',');
 			_putchar(' ');
 			if (k < 10)
 			{
 				_putchar(' ');
 				_putchar(k + '0');
+				
 			}
 			else
 			{
