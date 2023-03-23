d45089d243a2b770846b74f133c38e375abde285
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-03-23 14:39:03 +0300

    Task 0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - testd function that prints if integer is positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positve_or_negative(i);
+
+	return (0);
+}
