e70322fb5b7dcf7c50271d8852d2f0764437051c
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-03-23 14:54:40 +0300

    Fixed typo

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: check for positive or negative number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int i)
+{
+	/* your code goes there */
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
