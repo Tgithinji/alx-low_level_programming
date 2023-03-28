91868ea89b6927c0854fead219ef3f92e094a7cc
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-03-28 15:24:53 +0300

    Fixed output for strings with odd lengths

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,10 @@ void puts_half(char *str)
 	{
 		length++;
 	}
-	n = length / 2;
+	if (length % 2 == 0)
+		n = length / 2;
+	else
+		n = (length - 1) / 2;
 	for (i = n; i < length; i++)
 	{
 		_putchar(str[i]);
