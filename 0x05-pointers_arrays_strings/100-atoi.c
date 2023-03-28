f1e6fe9f7531e2739c2976f14a7de15d2c492357
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-03-28 23:37:59 +0300

    Try to fix integer overflow error

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,7 +20,7 @@ int _atoi(char *s)
 			sign = sign * -1;
 		if (s[i] >= 48 && s[i] <= 57)
 		{
-			num = num * 10 + s[i] - 48;
+			num = num * 10 + (s[i] - 48);
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 		}
