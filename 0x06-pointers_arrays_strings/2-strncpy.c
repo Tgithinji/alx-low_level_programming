712c002d95174498974ecb456978e5455344342d
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-03-30 13:18:11 +0300

    Copy strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string a number of bytes to another strin
+ * @dest: firt string
+ * @src: string to be copied
+ * @n: number of bytes to be copied
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
