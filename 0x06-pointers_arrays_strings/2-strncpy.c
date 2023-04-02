670e7fe509cb9b88c56fb15564bf1da9f594b5ce
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-04-02 22:17:21 +0300

    Fixed errors for Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,5 +18,11 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 	}
 
+	/* if loop stopped and did not reach n  continue*/
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
 	return (dest);
 }
