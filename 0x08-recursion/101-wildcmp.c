d7432b05e4a241cc3f78b617522bbb634dbf650d
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-04-06 23:20:06 +0300

    Fixed error from betty

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,10 +11,10 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '*')
 	{
-		if (*s1 != '\0' && wildcmp(s1 +1, s2))
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 			return (1);
 		else
-			return wildcmp(s1, s2 + 1);
+			return (wildcmp(s1, s2 + 1));
 	}
 
 	if (*s1 != *s2)
