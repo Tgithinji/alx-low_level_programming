ed9a4b2c5862e01776f208f82fd485f4b7ba9fd8
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-04-10 19:57:14 +0300

    Complete task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0 Always
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
