e43d0df068dea96346c38b1c0c3cf97a495b97bd
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-04-18 01:00:36 +0300

    Fixed errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
 
+int _strlen(char *s);
+char _strcpy(char *s1, char *s2);
 /**
  * new_dog - creates a new dog
  * @name: name of dog
@@ -16,8 +18,60 @@ dog_t *new_dog(char *name, float age, char *owner)
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
-	my_dog->name = name;
+	my_dog->name = malloc(_strlen(name) + 1);
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	my_dog->owner = malloc(_strlen(owner) + 1);
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+
+	_strcpy(my_dog->name, name);
 	my_dog->age = age;
-	my_dog->owner = owner;
+	_strcpy(my_dog->owner, owner);
 	return (my_dog);
+}
+
+/**
+ * _strlen - find the length of a string
+ * @s: the string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len, i;
+
+	len = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _strcpy - copies one string to another
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: pointer to s2
+ */
+char _strcpy(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		s1[i] = s2[i];
+	}
+	s1[i] = '\0';
+
+	return (*s1);
 }
