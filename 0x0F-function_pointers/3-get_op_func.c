560fef372ed8cdc9a127f51c39ee44d65795511e
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-04-20 03:38:59 +0300

    Create get_op_func.c file and update calc.h header file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - Returns a pointer to the function that
+ * corresponds to the operator given as parameter
+ * @s: the given operator
+ *
+ * Return: a pointer to corresponding function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*s == *(ops[i].op))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
