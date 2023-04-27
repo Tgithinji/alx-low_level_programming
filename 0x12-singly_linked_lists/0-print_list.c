e109159c3a4ee6ac08202a1eae00f6e7b6b735e3
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-04-28 00:08:55 +0300

    Create function to print elements of a node

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the first node
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	/* check if h is NULL */ 
+	if (h == NULL)
+		return (-1);
+
+	/* loop through each node and print its content */
+	for (count = 0; h != NULL; count++)
+	{
+		/* check if string is NULL */
+		if (h->str == NULL)
+		{
+			printf("[%d] (%s)\n", 0, "nil");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (count);
+}
