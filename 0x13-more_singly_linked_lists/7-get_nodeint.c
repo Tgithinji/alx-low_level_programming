47477199936f1d3f8de12a6624faa5d23917b4c3
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-05-02 00:12:06 +0300

    Fix output error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,9 +12,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current;
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
-
 	current = head;
 	/* loop upto the node at the given index */
 	for (i = 0; i < index; i++)
