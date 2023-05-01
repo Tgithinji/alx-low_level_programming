92cffcfc45b8e04d88aca7f9c7fac44e9352f543
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-05-02 01:43:14 +0300

    Try to fix output errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,15 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		current = current->next;
 	}
-
-	if (current == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
 		new_node->next = current->next;
 		current->next = new_node;
-	}
 	return (new_node);
 }
