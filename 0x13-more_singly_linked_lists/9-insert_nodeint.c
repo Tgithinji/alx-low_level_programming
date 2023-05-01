502e2410686175e6896ebac640b155efe8a2c513
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-05-02 01:41:12 +0300

    Try to fix output errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 	/* loop to the position the node should be added */
-	for (i = 0; i < idx - 1; i++)
+	for (i = idx - 1; i != 0; i--)
 	{
 		current = current->next;
 	}
