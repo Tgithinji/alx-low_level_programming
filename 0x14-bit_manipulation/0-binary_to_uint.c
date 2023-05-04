91ef1e53e943a5050414638baedc989c604f30be
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-05-05 00:58:50 +0300

    Function to find decimal from binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+/**
+ * binary_t0_uint: Function that converts a binary number to unsigned int
+ * @b: points to a string og '0' ang '1'
+ *
+ * Return: the converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int j, num, dec_num;
+
+	if (b == NULL)
+		return(0);
+
+	dec_num = 0;
+
+	for (j = 0; *(b + j) != '\0'; j++)
+	{
+		num = *(b + j) - '0';
+		if (num == 1 || num == 0)
+		{
+			dec_num = (dec_num << 1) | num;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (dec_num);
+}
