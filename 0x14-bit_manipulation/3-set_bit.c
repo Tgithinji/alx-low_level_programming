e1c3341697b456c29696248cec2fd477e7f45efa
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-05-05 03:30:46 +0300

    Set the nth bit of a number

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: the number whose bit is to be set
+ * @index: the index at which 1 is to be set
+ *
+ * Result: 1 (success). -1 (failed)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+		return (-1);
+
+	/* left shift 1 index times and perform OR operator with n */
+	*n = (1 << index) | *n;
+
+	return (1);
+}
