d27373d6f40a7a2ae87694c08a5c41f4a8791bbb
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-05-09 02:05:33 +0300

    Fix output error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,16 +18,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* create and open file */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	/* find length of text content */
 	len = 0;
-	while (*text_content != '\0')
+	while (*(text_content + len) != '\0')
 	{
 		len++;
-		text_content++;
 	}
 
 	/* write to the file */
