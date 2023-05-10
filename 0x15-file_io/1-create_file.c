bf0ea5547ff124440e155c96b6f1dd203970aa24
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-05-10 23:41:13 +0300

    Fix output error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	/* create and open file */
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
