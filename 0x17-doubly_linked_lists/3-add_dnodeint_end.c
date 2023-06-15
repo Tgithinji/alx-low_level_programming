cdeb51596a446948eb200c7157d2c45de7c14679
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-06-15 22:06:48 +0300

    Function that adds node at the end of a list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a node list
+ * @head: first node in the list
+ * @n: data for the new node list
+ *
+ * Return: address to new element
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *temp;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if ((*head) == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
+	return (new_node);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -8,3 +8,6 @@ Write a function that returns the number of elements in a double linked list.
 ## Task 2
 Write a function that adds a new node at the beginning of a doubly linked list.
 - Filename: `2-add_dnodeint.c`
+## Task 3
+Write a function that adds a new node at the end of double list
+- Filename: `3-add_dnodeint.c`
