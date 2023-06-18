ec2cfae9d5935ada77f7e381dcfcc9caf114632e
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-06-18 11:38:58 +0300

    Handle the edge case of inserting a node to two elements

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,24 +24,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
 		*h = new_node;
 	}
-	else if (idx > 0)
+	else
 	{
-		while (idx != 1)
+		while (idx != 0 && temp != NULL)
 		{
 			temp = temp->next;
 			idx--;
 		}
-		temp2 = temp->next;
-		temp->next = new_node;
-		temp2->prev = new_node;
-		new_node->prev = temp;
-		new_node->next = temp2;
-	}
-	else
-	{
-		return (NULL);
+		if (idx > 0)
+		{
+			return (NULL);
+		}
+		if (temp != NULL)
+		{
+			temp2 = temp->next;
+			temp->next = new_node;
+			temp2->prev = new_node;
+			new_node->prev = temp;
+			new_node->next = temp2;
+		}
+		else
+		{
+			return (NULL);
+		}
 	}
 	return (new_node);
 }
