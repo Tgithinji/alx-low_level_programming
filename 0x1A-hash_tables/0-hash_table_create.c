ca3b6bc8b8589c6ffddc6e52c6092c9531600fa4
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-07-10 00:08:43 +0300

    Hash function implementing the djb2 algorithm

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,6 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
@@ -20,9 +19,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_table_t) * table->size);
 	if (table->array == NULL)
 		return (NULL);
-	for (i = 0; i < table->size; i++)
-	{
-		table->array[i] = NULL;
-	}
+
 	return (table);
 }
